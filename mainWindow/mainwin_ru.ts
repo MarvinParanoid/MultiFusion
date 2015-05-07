272f18204dddd3c2a855dcadd369555d69365338
MarvinParanoid <worialhat@gmail.com>
2015-05-07 14:14:26 +0400

    add

---
+++ mainWindow/mainwin_ru.ts
@@ -0,0 +1,340 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message>
+        <location filename="mainWindow.cpp" line="599"/>
+        <source>MultiFusion application</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="605"/>
+        <source>Exit from MultiFusion.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="606"/>
+        <source>Are you shure to exit from MultiFusion?</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>mainWindow</name>
+    <message>
+        <location filename="mainWindow.cpp" line="17"/>
+        <source>&amp;File</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="18"/>
+        <source>Main toolbar</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="21"/>
+        <source>&amp;Edit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="25"/>
+        <source>&amp;Tools</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="26"/>
+        <source>&amp;Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="28"/>
+        <source>&amp;Plugins...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="32"/>
+        <source>Show viewport properties...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="36"/>
+        <source>&amp;Help...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="39"/>
+        <source>About &amp;Qt...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="43"/>
+        <source>&amp;About...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="46"/>
+        <location filename="mainWindow.cpp" line="261"/>
+        <source>:Untitled</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="57"/>
+        <source>&amp;New</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="60"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="67"/>
+        <source>&amp;Open...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="69"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="79"/>
+        <source>&amp;Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="83"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="88"/>
+        <source>Save &amp;as...</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="93"/>
+        <source>&amp;Exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="97"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="101"/>
+        <location filename="mainWindow.cpp" line="195"/>
+        <source>To spline</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="102"/>
+        <source>Show Bezier points</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="104"/>
+        <source>&amp;Delete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="108"/>
+        <source>C&amp;ut</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="110"/>
+        <source>&amp;Copy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="112"/>
+        <source>&amp;Paste</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="117"/>
+        <source>&amp;Group</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="118"/>
+        <source>&amp;Ungroup</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="130"/>
+        <source>Ctrl+G</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="131"/>
+        <source>Ctrl+U</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="133"/>
+        <source>Del</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="134"/>
+        <source>Ctrl+X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="135"/>
+        <source>Ctrl+C</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="136"/>
+        <source>Ctrl+V</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="193"/>
+        <source>To normal</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="207"/>
+        <source>Hide bezier points</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="209"/>
+        <source>Show bezier points</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="229"/>
+        <source>Cut selection to clipboard.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="230"/>
+        <source>Are you shure to cut this layers to clipboard?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="251"/>
+        <source>Create new animation.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="252"/>
+        <source>Are you shure to create new animation?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="253"/>
+        <location filename="mainWindow.cpp" line="269"/>
+        <source>It delete loaded film, if it unsaved.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="267"/>
+        <source>Loading animation.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="268"/>
+        <source>Are you shure to load animation?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="277"/>
+        <source>Open SACS2 animation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="278"/>
+        <source>SACS2 animation (*.sacs2);;All files (*)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="286"/>
+        <location filename="mainWindow.cpp" line="320"/>
+        <location filename="mainWindow.cpp" line="348"/>
+        <source>Can&apos;t open file.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="287"/>
+        <source>Can&apos;t open selected file to reading.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="288"/>
+        <location filename="mainWindow.cpp" line="322"/>
+        <location filename="mainWindow.cpp" line="350"/>
+        <source>May be you don&apos;t have permissions?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="298"/>
+        <source>Error reading animation.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="299"/>
+        <source>Data is corrupt or saved in unsupported format.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="305"/>
+        <location filename="mainWindow.cpp" line="359"/>
+        <source>:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="321"/>
+        <location filename="mainWindow.cpp" line="349"/>
+        <source>Can&apos;t open selected file to writing.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="335"/>
+        <source>Save SACS2 animation</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="336"/>
+        <source>SACS2 animation (*.sacs2)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="377"/>
+        <source>Deleting current frame.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="378"/>
+        <source>Are you shure to delete this frame?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="582"/>
+        <source>Help</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="582"/>
+        <source>Currently not implemented :)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="589"/>
+        <source>About MultiFusion</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainWindow.cpp" line="590"/>
+        <source>&lt;CENTER&gt;&lt;B&gt;MultiFusion&lt;/B&gt; super-puper application.
+</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
